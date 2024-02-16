471a31ed72f5f8484034594baf430794e69c2326
Aditya <akaditya394@gmail.com>
2024-02-16 16:47:55 +0530

    unique id function

---
+++ src/helper/generateId.js
@@ -0,0 +1,11 @@
+export default function generateRandomId() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomId = "";
+  for (let i = 0; i < 10; i++) {
+    randomId += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return randomId;
+}

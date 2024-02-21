ac7a218e7501047649750af1096dbd56c56f21e6
Aditya <akaditya394@gmail.com>
2024-02-21 22:17:31 +0530

    formatted date string

--- src/helper/convertTimestampToString.js
+++ src/helper/convertTimestampToString.js
@@ -3,18 +3,20 @@ export function convertTimestampToString(timestamp) {
     timestamp.seconds * 1000 + timestamp.nanoseconds / 1e6;
 
   const date = new Date(totalMilliseconds);
-
-  const formattedString =
-    date.toLocaleString("en-US", {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-      hour12: true,
-      timeZone: "UTC",
-    }) + "+5:30UTC";
+  const formattedString = formatDateString(date);
 
   return formattedString;
+}
+
+function formatDateString(date) {
+  const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date
+    .getUTCDate()
+    .toString()
+    .padStart(2, "0")} at ${date.getHours().toString().padStart(2, "0")}:${date
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}`;
+  return formattedDate;
 }
